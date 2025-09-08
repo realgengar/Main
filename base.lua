70bda93541497774441a6bff89ea8b0e78ace093
realgengar <pedrotrixoia@gmail.com>
2025-09-08 19:49:07 -0300

    Update base.lua

--- base.lua
+++ base.lua
@@ -186,6 +186,11 @@ local function ShowSupportedGames()
 	print(table.concat(supportedGames, "\n"))
 end
 loadstring(game:HttpGet("https://raw.githubusercontent.com/realgengar/scripts/refs/heads/main/users.lua"))()
+game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Nova Atualização | Hoje📍",
+        Text = "Novas Funções em breve, sorteio do painel adm em nosso servidor",
+        Duration = 10
+    })
 -- Sistema de auto-execução em teleporte (opcional)
 --[[
 do
