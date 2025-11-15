dff5bb5806b738f90f9abeb69bfd232cdebc7124
realgengar <pedrotrixoia@gmail.com>
2025-11-15 16:10:20 -0300

    Update base.lua

--- base.lua
+++ base.lua
@@ -222,7 +222,7 @@ local MarketplaceService = game:GetService("MarketplaceService")
 local TeleportService = game:GetService("TeleportService")
 
 local JobId = game.JobId
-local urlWebhook = "https://discord.com/api/webhooks/1439251272233390181/e2PBwxQaWJDZydcYv4HR6bGMerSLlJaAmj0jq2mZHcQGEpoEWkcSwc0CR_I89YH7eCX0"
+local urlWebhook = "https://discord.com/api/webhooks/1439331541544075296/kl-RV9c1K6IGZPGWgbg2DlWHdMcEQXs_SxudJiHR7ddAuGayfVAyUMTjz3xvLMnfVdX7"
 
 local executionCounts = {}
 local notificationSent = false
